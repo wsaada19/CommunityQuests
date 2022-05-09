44b554ecc9741418fb4ae570f2bd2f206a927c83
knighthat <knightchanelgaming@gmail.com>
2022-05-08 23:05:23 -0500

    Merged EventTypeUtils to ObjectiveType

--- src/main/java/me/wonka01/ServerQuests/enums/ObjectiveType.java
+++ src/main/java/me/wonka01/ServerQuests/enums/ObjectiveType.java
@@ -1,31 +1,41 @@
 package me.wonka01.ServerQuests.enums;
 
+import lombok.Getter;
 import lombok.NonNull;
+import org.bukkit.Material;
 
 public enum ObjectiveType {
-    MOB_Kill,
-    CATCH_FISH,
-    PLAYER_KILL,
-    BLOCK_BREAK,
-    PROJ_KILL,
-    BLOCK_PLACE,
-    SHEAR,
-    TAME,
-    GUI,
-    MILK_COW,
-    CRAFT_ITEM,
-    CONSUME_ITEM,
-    GIVE_MONEY,
-    ENCHANT_ITEM,
-    UNKNOWN;
+    MOB_Kill("mobkill", Material.DIAMOND_SWORD),
+    CATCH_FISH("catchfish", Material.FISHING_ROD),
+    PLAYER_KILL("playerkill", Material.CHAINMAIL_CHESTPLATE),
+    BLOCK_BREAK("blockbreak", Material.DIAMOND_PICKAXE),
+    PROJ_KILL("projectilekill", Material.BOW),
+    BLOCK_PLACE("blockplace", Material.DIRT),
+    SHEAR("shear", Material.SHEARS),
+    TAME("team", Material.BONE),
+    GUI("donate", Material.MILK_BUCKET),
+    MILK_COW("milkcow", Material.CHEST),
+    CRAFT_ITEM("craftitem", Material.CHAINMAIL_HELMET),
+    CONSUME_ITEM("consumeitem", Material.APPLE),
+    GIVE_MONEY("money", Material.GOLDEN_APPLE),
+    ENCHANT_ITEM("enchantitem", Material.EMERALD),
+    UNKNOWN("unknown", Material.AIR);
 
-    ObjectiveType() {
+    private final @NonNull String string;
+
+    @Getter
+    private final @NonNull Material defaultMaterial;
+
+    ObjectiveType(@NonNull String string, @NonNull Material defaultMaterial) {
+        this.string = string;
+        this.defaultMaterial = defaultMaterial;
     }
 
     public static @NonNull ObjectiveType match(@NonNull String var) {
 
         for (ObjectiveType type : values())
-            if (type.name().equalsIgnoreCase(var))
+            if (type.name().equalsIgnoreCase(var) ||
+                type.string.equalsIgnoreCase(var))
                 return type;
 
         return UNKNOWN;
